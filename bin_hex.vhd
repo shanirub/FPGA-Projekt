4e145f4906bd998771c14e603b2e43eadf95d7e4
Shani Rub <43911148+shanirub@users.noreply.github.com>
2020-02-11 15:01:29 +0100

    all codes options added

--- bin_hex.vhd
+++ bin_hex.vhd
@@ -30,7 +30,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity bin_system is
-	Port ( 	A : in  STD_LOGIC_VECTOR(5 downto 0); -- 4 bit Nummer + 2 bit Funktion Auswahl
+	Port ( 	A : in  STD_LOGIC_VECTOR(5 downto 0); -- 6bit = 4 bit Nummer + 2 bit Funktion Auswahl
 			SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G : out  STD_LOGIC; -- 7 bit, Ausgabe in 7-Segment Anzeige als Leds
 		);
 end bin_system;
@@ -63,7 +63,7 @@ signal OUTS0 : STD_LOGIC_VECTOR(6 downto 0);
 
 begin
   with A select -- 4bit code + 2bit funktion
-  	OUTS0 <= "seg_wert_0" when "0000",
+  		OUTS0 <=	"seg_wert_0" when "0000" & bcd,
 				"seg_wert_1" when "0001" & bcd,
 				"seg_wert_2" when "0010" & bcd,
 				"seg_wert_3" when "0011" & bcd,
@@ -73,6 +73,41 @@ begin
 				"seg_wert_7" when "0111" & bcd,
 				"seg_wert_8" when "1000" & bcd,
 				"seg_wert_9" when "1001" & bcd,
+	
+				"seg_wert_0" when "0000" & aiken,
+				"seg_wert_1" when "0001" & aiken,
+				"seg_wert_2" when "0010" & aiken,
+				"seg_wert_3" when "0011" & aiken,
+				"seg_wert_4" when "0100" & aiken,
+				"seg_wert_5" when "1011" & aiken,
+				"seg_wert_6" when "1100" & aiken,
+				"seg_wert_7" when "1101" & aiken,
+				"seg_wert_8" when "1110" & aiken,
+				"seg_wert_9" when "1111" & aiken,
+	
+				"seg_wert_0" when "0011" & excess_3,
+				"seg_wert_1" when "0100" & excess_3,
+				"seg_wert_2" when "0101" & excess_3,
+				"seg_wert_3" when "0110" & excess_3,
+				"seg_wert_4" when "0111" & excess_3,
+				"seg_wert_5" when "1000" & excess_3,
+				"seg_wert_6" when "1001" & excess_3,
+				"seg_wert_7" when "1010" & excess_3,
+				"seg_wert_8" when "1011" & excess_3,
+				"seg_wert_9" when "1100" & excess_3,
+	
+				"seg_wert_0" when "0000" & gray,
+				"seg_wert_1" when "0001" & gray,
+				"seg_wert_2" when "0011" & gray,
+				"seg_wert_3" when "0010" & gray,
+				"seg_wert_4" when "0110" & gray,
+				"seg_wert_5" when "0111" & gray,
+				"seg_wert_6" when "0101" & gray,
+				"seg_wert_7" when "0100" & gray,
+				"seg_wert_8" when "1100" & gray,
+				"seg_wert_9" when "1101" & gray,
+
+			
 				"seg_wert_error" when others;	-- the "others" clause: shows an error
 				
   SEG0A <= OUTS0(6); SEG0B <= OUTS0(5); SEG0C <= OUTS0(4); SEG0D <= OUTS0(3);
